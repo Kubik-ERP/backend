a2b9a0f69445b9abafd744b6a3f91bab4d82e403
aaronmacauley <89644550+aaronmacauley@users.noreply.github.com>
2025-04-08 23:01:28 +0700

    tes

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
-import { CustomerController } from './customer.controller';
+import { CustomersController } from './customer.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CategoriesController } from '../categories/categories.controller';
 import { CategoriesService } from '../categories/categories.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [CustomerController],
+  controllers: [CustomersController],
   providers: [CustomerService],
   exports: [CustomerService],
 })
