71c19c51fac2ede4f3847151b7800b51be4bc81f
aaronmacauley <89644550+aaronmacauley@users.noreply.github.com>
2025-04-17 02:00:49 +0700

    fix variant

--- src/modules/variants/variants.controller.ts
+++ src/modules/variants/variants.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { VariantsService } from './variants.service';
 import { CreateVariantDto } from './dto/create-variant.dto';
@@ -16,27 +18,120 @@ export class VariantsController {
   constructor(private readonly variantsService: VariantsService) {}
 
   @Post()
-  create(@Body() createVariantDto: CreateVariantDto) {
-    return this.variantsService.create(createVariantDto);
+  async create(@Body() createVariantDto: CreateVariantDto) {
+    try {
+      const newVariant = await this.variantsService.create(createVariantDto);
+      return {
+        statusCode: 201,
+        message: 'Variant created successfully',
+        result: newVariant,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        message: error.message,
+        result: null,
+      };
+    }
   }
 
   @Get()
-  findAll() {
-    return this.variantsService.findAll();
+  async findAll() {
+    try {
+      const variants = await this.variantsService.findAll();
+      return { statusCode: 200, message: 'Success', result: variants };
+    } catch (error) {
+      console.error('Error fetching variants:', error);
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Failed to fetch variants',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.variantsService.findOne(+id);
+  async findOne(@Param('idOrName') idOrName: string) {
+    try {
+      const variant = await this.variantsService.findOne(idOrName);
+      if (!variant) {
+        throw new HttpException(
+          { statusCode: HttpStatus.NOT_FOUND, message: 'variant not found' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return { statusCode: 200, message: 'Success', result: variant };
+    } catch (error) {
+      console.error('Error finding variant:', error);
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Failed to fetch variant',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVariantDto: UpdateVariantDto) {
-    return this.variantsService.update(+id, updateVariantDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateVariantDto: UpdateVariantDto,
+  ) {
+    try {
+      const updatedVariant = await this.variantsService.update(
+        id,
+        updateVariantDto,
+      );
+
+      if (!updatedVariant) {
+        throw new HttpException(
+          { statusCode: HttpStatus.NOT_FOUND, message: 'Variant not found' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return {
+        statusCode: 200,
+        message: 'variant updated successfully',
+        result: updatedVariant,
+      };
+    } catch (error) {
+      console.error('Error updating variant:', error);
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: error.message || 'Failed to update variant',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.variantsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    try {
+      const deletedVariant = await this.variantsService.remove(id);
+
+      if (!deletedVariant) {
+        throw new HttpException(
+          { statusCode: HttpStatus.NOT_FOUND, message: 'Variant not found' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return { statusCode: 200, message: 'Variant deleted successfully' };
+    } catch (error) {
+      console.error('Error deleting Variant:', error);
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Failed to delete Variant',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
