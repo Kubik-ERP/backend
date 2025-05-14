366604fc02d22a7c2b90e63598a0d24d81ac742d
Abdur Rahimi <abdurrahimi22@gmail.com>
2025-05-14 11:11:49 +0700

    lint

--- src/modules/authentication/dtos/register.dto.ts
+++ src/modules/authentication/dtos/register.dto.ts
@@ -19,11 +19,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Match } from 'src/common/helpers/validators.helper';
 
 export class RegisterEmailDto {
-
   @ApiProperty()
   @IsNotEmpty()
   public fullName: string;
-
 
   @ApiProperty()
   @IsNotEmpty()
