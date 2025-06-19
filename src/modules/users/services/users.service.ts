f69ea1fa1726ff2d834d14d684b9005075cd2c97
Abdur Rahimi <abdurrahimi22@gmail.com>
2025-06-19 15:36:19 +0700

    lint

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -112,11 +112,11 @@ export class UsersService {
    * @description Find user by ID
    */
   public async findOneById(id: number): Promise<UserModel> {
-    const user = await this.prisma.users.findUnique({ 
+    const user = await this.prisma.users.findUnique({
       where: { id },
       include: {
         roles: true, // Include roles if needed
-      }
+      },
     });
 
     if (!user) {
