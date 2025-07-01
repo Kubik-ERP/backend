72cabde3dc64f7c0bd0306e447427d9c6090ec65
Muhammad Reza Pahlevi Y <muhammad.reza.pahlevi.y@gmail.com>
2025-07-01 21:15:48 +0700

    feat: new module kitchen

--- src/modules/kitchen/controllers/kitchen-queue.controller.ts
+++ src/modules/kitchen/controllers/kitchen-queue.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthenticationJWTGuard } from 'src/common/guards/authentication-jwt.guard';
 
 @Controller('kitchen')
-export class KitchenController {}
+export class KitchenController {
+  constructor() {
+    //
+  }
+
+  @UseGuards(AuthenticationJWTGuard)
+  @Get('ticket/:invoiceId')
+  @ApiOperation({
+    summary: 'Get Kitchen Ticket in Invoice Detail',
+  })
+  public async getTicketByInvoiceId(@Param('invoiceId') invoiceId: string) {
+    // Implement your logic here
+  }
+}
--- src/modules/kitchen/kitchen.module.ts
+++ src/modules/kitchen/kitchen.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { KitchenController } from './controllers/kitchen-queue.controller';
 import { KitchenService } from './services/kitchen.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  controllers: [KitchenController],
   providers: [KitchenService, PrismaService],
   exports: [PrismaService],
 })
