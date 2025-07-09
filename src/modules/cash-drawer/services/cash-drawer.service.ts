12c12c743ee6bf47694bcebeca0dd5f60029ac0f
Abdur Rahimi <abdurrahimi22@gmail.com>
2025-07-09 09:54:38 +0700

    fix: column data for cash drawers

--- src/modules/cash-drawer/services/cash-drawer.service.ts
+++ src/modules/cash-drawer/services/cash-drawer.service.ts
@@ -195,6 +195,7 @@ export class CashDrawerService {
           amount_in: amountIn,
           amount_out: amountOut,
           final_amount: updatedBalance,
+          notes: notes || '',
           created_by: userId,
           created_at: jakartaTime().toUnixInteger(),
         },
@@ -229,6 +230,14 @@ export class CashDrawerService {
 
     const [transactions, count] = await Promise.all([
       this.prisma.cash_drawer_transactions.findMany({
+        include: {
+          users: {
+            select: {
+              id: true,
+              fullname: true
+            }
+          }
+        },
         where,
         orderBy: { created_at: 'desc' },
         take: limit,
