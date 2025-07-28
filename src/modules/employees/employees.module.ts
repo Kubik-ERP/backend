2373ba29d5999a66ac93ba01a1582e7e9e5bf00b
aaronmacauley <89644550+aaronmacauley@users.noreply.github.com>
2025-07-28 13:05:48 +0700

    Fix/staff (#140)
    * fix stagging error + filter employees name, title ,permissions
    
    * revert config
    
    * add revision Mas Aldo

--- src/modules/employees/employees.module.ts
+++ src/modules/employees/employees.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { StorageService } from '../storage-service/services/storage-service.service';
+import { StorageServiceModule } from '../storage-service/storage-service.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StorageServiceModule],
   controllers: [EmployeesController],
   providers: [EmployeesService],
 })
