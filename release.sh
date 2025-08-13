970b823709a2342e5342862d02570b028f96186e
abdurrahimi <abdurrahimi22@gmail.com>
2025-08-13 17:29:18 +0700

    update

--- release.sh
+++ release.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 set -eu
 
-# ===== CONFIG =====
+APP_NAME="pos-backend"
 VERSION="${1:-}"
+USERNAME="abdurrahimi"
+REGISTRY="ghcr.io"
+IMAGE="$REGISTRY/$USERNAME/$APP_NAME"
 
+# VALIDASI
 if [ -z "$VERSION" ]; then
-    echo "Usage: sh release.sh <version>"
-    exit 1
+  echo "Usage: sh deploy.sh <version>"
+  exit 1
+fi
+if [ -z "${GHCR_TOKEN:-}" ]; then
+  echo "Error: GHCR_TOKEN environment variable is not set."
+  exit 1
 fi
 
-# ===== GIT TAG =====
-echo "=== Creating git tag v$VERSION ==="
-git tag -a "v$VERSION" -m "Release version $VERSION"
+# BUILD & PUSH
+docker build -t "$IMAGE:$VERSION" -t "$IMAGE:latest" .
+echo "$GHCR_TOKEN" | docker login "$REGISTRY" -u "$USERNAME" --password-stdin
+docker push "$IMAGE:$VERSION"
+docker push "$IMAGE:latest"
+
+# GIT TAG
+git tag -a "v$VERSION" -m "Release $VERSION"
 git push origin "v$VERSION"
 
-
+echo "=== Release $VERSION done ==="
