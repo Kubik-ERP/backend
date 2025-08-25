56d7fcbeb41a2e3d0a2e75ea56c0bfcac2688358
Bil Abror <bilabror@gmail.com>
2025-08-25 20:33:02 +0700

    ref(rbac): implement in vouchers module.

--- src/modules/vouchers/vouchers.module.ts
+++ src/modules/vouchers/vouchers.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { VouchersService } from './vouchers.service';
 import { VouchersController } from './vouchers.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
@@ -6,6 +7,6 @@ import { PrismaModule } from '../../prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   controllers: [VouchersController],
-  providers: [VouchersService],
+  providers: [VouchersService, Reflector],
 })
 export class VouchersModule {}
