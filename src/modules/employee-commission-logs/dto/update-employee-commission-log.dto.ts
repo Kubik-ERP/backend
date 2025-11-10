6e34dbf7fb7c32b1e41906e207396d314f462605
Bil Abror <bilabror@gmail.com>
2025-11-10 23:01:03 +0700

    chore: formatting.

--- src/modules/employee-commission-logs/dto/update-employee-commission-log.dto.ts
+++ src/modules/employee-commission-logs/dto/update-employee-commission-log.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateEmployeeCommissionLogDto } from './create-employee-commission-log.dto';
 
-export class UpdateEmployeeCommissionLogDto extends PartialType(CreateEmployeeCommissionLogDto) {}
+export class UpdateEmployeeCommissionLogDto extends PartialType(
+  CreateEmployeeCommissionLogDto,
+) {}
